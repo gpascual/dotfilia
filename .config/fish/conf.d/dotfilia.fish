84e772607242fca4f97198d25e4da767e8c1ceff
Gonzalo Pascual <gonzalo.pascual@lifullconnect.com>
2020-05-14 10:03:24 +0200

    fix: use /home/gpascual instead of assuming /home/gonzalo homedir when defining dotfilia alias

--- .config/fish/conf.d/dotfilia.fish
+++ .config/fish/conf.d/dotfilia.fish
@@ -1,4 +1,4 @@
-alias dotfilia='/usr/bin/git --git-dir=/home/gonzalo/.local/.dotfilia.git/ --work-tree=/home/gonzalo'
+alias dotfilia='/usr/bin/git --git-dir=$HOME/.local/.dotfilia.git/ --work-tree=$HOME'
 alias dotfilia-untrack-fish-variables='dotfilia update-index --assume-unchanged $HOME/.config/fish/fish_variables'
 alias dotfilia-track-fish-variables='dotfilia update-index --no-assume-unchanged $HOME/.config/fish/fish_variables'
 
