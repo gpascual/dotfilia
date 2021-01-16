98a6f35068e5dc41008da508fdd6859abb63f9bf
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2021-01-16 23:48:05 +0100

    feat: add alias to manage private dotfiles repository and remove fish variables (un)tracking aliases

--- .config/fish/conf.d/dotfilia.fish
+++ .config/fish/conf.d/dotfilia.fish
@@ -1,6 +1,6 @@
+# public dotfilia repo
 alias dots='/usr/bin/git --git-dir=$HOME/.local/dotfilia.git/ --work-tree=$HOME'
-alias dots-untrack-fish-variables='dots update-index --assume-unchanged $HOME/.config/fish/fish_variables'
-alias dots-track-fish-variables='dots update-index --no-assume-unchanged $HOME/.config/fish/fish_variables'
 
-dots-untrack-fish-variables
+# private dotfilia repo
+alias asterisks='/usr/bin/git --git-dir=$HOME/.local/asterisks.git/ --work-tree=$HOME'
 
