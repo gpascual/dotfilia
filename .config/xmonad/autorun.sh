74fa0d1dbc4f86d933e82ab15e411564809d5d01
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2022-07-14 12:09:43 +0200

    feat: allow adding custom Xmonad programs to run at startup inside an autorun.d folder

--- .config/xmonad/autorun.sh
+++ .config/xmonad/autorun.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-function run_once {
-  if ! pidof $1 ;
-  then
-    $@&
-  fi
-}
-
-function replace {
-  killall -e $1
-  $@&
-}
+. $XMONAD_CONFIG_HOME/autorun-functions
 
 # XDG desktop portal backend
 #run_once /usr/lib/xdg-desktop-portal
@@ -36,9 +26,20 @@ replace nitrogen-folder $XDG_DATA_HOME/backgrounds/black_and_white_architecture/
 
 # Third party Applets and Widgets
 run_once nm-applet                          # network
+run_once pa-applet                          # audio controls
 run_once pasystray -m 100 -i 1 --notify=all # audio controls
+systemctl is-active --quiet bluetooth \
+  && run_once blueman-applet                # bluetooth
 
 # Startup apps
 run_once jetbrains-toolbox --minimize %u
 run_once conky -c $HOME/.config/conky/xmonad.conkyrc
+
+# Run other programs from files inside autorun.d folder
+if [ -d $XMONAD_CONFIG_HOME/autorun.d ] ; then
+  for f in $XMONAD_CONFIG_HOME/autorun.d/?[!.]* ; do
+    . "$f"
+  done
+  unset f
+fi
 
