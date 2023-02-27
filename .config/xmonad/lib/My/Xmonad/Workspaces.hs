b58d589b9031bfdee64f623fe6631662a25f7fe0
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2023-02-27 23:18:26 +0100

    feat: updates nerd icons in xmonad workspaces names and tweaks xmobar font sizes

--- .config/xmonad/lib/My/Xmonad/Workspaces.hs
+++ .config/xmonad/lib/My/Xmonad/Workspaces.hs
@@ -12,11 +12,11 @@ names :: [String]
 names = ["shell", "code", "docs", "chat", "tasks", "web", "data", "misc"]
 
 icons :: [String]
-icons = ["\xe795", "\xf121", "\xfad9", "\xfa00", "\xf45e", "\xfa9e", "\xe706", "\xf8d5"]
+icons = ["\xf120", "\xe796", "\xf05da", "\xe217", "\xf0135", "\xf059f", "\xf163a", "\xf03d6"]
 
 myWorkspaces :: [String]
 myWorkspaces = clickable . (map xmobarEscape) $ names where
-        clickable names = [ "<action=xdotool key super+" ++ show index ++ "><fn=1>" ++ icon ++ " </fn>" ++ name ++ "</action>" |
+        clickable names = [ "<action=xdotool key super+" ++ show index ++ "><fn=1>" ++ icon ++ "</fn> " ++ name ++ "</action>" |
                       (index, name, icon) <- zip3 [1..8] names icons]
 
 getWs :: String -> String
