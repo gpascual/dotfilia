2796ca917678a76d906c19fd2ec82c875ca38830
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2024-03-06 17:29:58 +0100

    feat: fixes xmonad startup hook do indentation

--- .config/xmonad/lib/My/Xmonad/StartupHook.hs
+++ .config/xmonad/lib/My/Xmonad/StartupHook.hs
@@ -16,11 +16,11 @@ addNETSupported x   = withDisplay $ \dpy -> do
   a_NET_SUPPORTED <- getAtom "_NET_SUPPORTED"
   a               <- getAtom "ATOM"
   liftIO $ do
-  sup <- (join . maybeToList) <$> getWindowProperty32 dpy a_NET_SUPPORTED r
-  when (fromIntegral x `notElem` sup) $ changeProperty32 dpy r a_NET_SUPPORTED a propModeAppend [fromIntegral x]
+    sup <- (join . maybeToList) <$> getWindowProperty32 dpy a_NET_SUPPORTED r
+    when (fromIntegral x `notElem` sup) $ changeProperty32 dpy r a_NET_SUPPORTED a propModeAppend [fromIntegral x]
 
 addEWMHFullscreen :: X ()
-addEWMHFullscreen   = do
+addEWMHFullscreen = do
   wms <- getAtom "_NET_WM_STATE"
   wfs <- getAtom "_NET_WM_STATE_FULLSCREEN"
   mapM_ addNETSupported [wms, wfs]
